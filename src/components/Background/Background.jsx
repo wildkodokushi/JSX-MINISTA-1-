0f348c6f17cdf8dd618dd1b2b3db99baab70489f
vlad bronnikov <bronnikovvd@gmail.com>
2026-08-18 03:02:01 +0300

    feat: create select project block

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -2,11 +2,11 @@ import './Background.scss'
 import clsx from 'clsx'
 
 export default (props) => {
-    const { className, direction, behavior, scrollamount, text } = props
+    const { className, direction, text } = props
 
     return (
         <>
-            <marquee className={clsx('marquee', className)} behavior={behavior} direction={direction} scrollamount={scrollamount}>
+            <marquee className={clsx('marquee', className)} aria-hidden="true" behavior="scroll" direction={direction} scrollamount="20">
                 {text}
             </marquee>
         </>
--- src/sections/Select/Select.jsx
+++ src/sections/Select/Select.jsx
@@ -1,9 +1,18 @@
 import './Select.scss'
+import Background from '@/components/Background'
 
 export default () => {
     return (
         <>
-            
+            <div className="select">
+                <div className="select__container container">
+                    <h5 className="select__container-subtitle h5">select project</h5>
+                    <h2 className="select__container-title h2">
+                        browse popular houses based on their features.
+                    </h2>
+                </div>
+                <Background className="select__background" direction="left" text="select"></Background>  
+            </div>
         </>
     )
 }
