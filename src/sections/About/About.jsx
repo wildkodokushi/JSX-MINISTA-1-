48a64d69cda62c96a16a83c10ceb2edfd098f7b0
vlad bronnikov <bronnikovvd@gmail.com>
2026-08-18 03:02:01 +0300

    feat: put together a comprehensive 'About Us' section

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,6 +1,7 @@
 import Trust from "@/sections/About/Trust"
 import Select from "@/sections/About/Select"
 import Project from "@/sections/About/Project"
+import './About.scss'
 
 export default () => {
     return (
