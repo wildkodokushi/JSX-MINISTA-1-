c438ffaf9b5922d021077b3fd3f198bf00503761
vlad bronnikov <bronnikovvd@gmail.com>
2026-08-19 19:01:24 +0300

    feat: The user agreement is now saved to localStorage.

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -3,19 +3,15 @@ import clsx from 'clsx'
 
 export default (props) => {
     const { notification, status, id } = props
-    const stateClasses = {
-        disallow: 'disallow',
-        allow: 'allow',
-    }
-
-    status === 'disallow' ? stateClasses.disallow : stateClasses.allow
 
     return (
         <>
-            <div className="toast">
+            <div className="toast" id={id}>
                 <div className="toast__container">
-                    <div className={clsx("toast__container-notification", status)} id={id}>
-                        {notification}
+                    <div className={clsx("toast__container-notification", status)}>
+                        <span className="notification">
+                            {notification}
+                        </span>
                     </div>
                 </div>
             </div>
--- src/modules/NotificationPush.js
+++ src/modules/NotificationPush.js
@@ -1,12 +1,48 @@
-import Toast from "@/components/Toast"
+const trustCtaElement = document.querySelector('.trust__cta')
 
-const toastElement = document.querySelector('.toast')
+const toastAllowElement = document.getElementById('status-allow')
+const toastDisallowElement = document.getElementById('status-disallow')
+
 const allowButton = document.querySelector('.allow')
 const disallowButton = document.querySelector('.disallow')
 
+if(localStorage.getItem('hideTrustCta') === 'true') {
+    trustCtaElement.classList.add('visually-hidden')
+}
+
 export default function clickHandler() {
     disallowButton.addEventListener('click', () => {
+        const notification = 'DISALLOW'
+        const notificationElement = toastDisallowElement.querySelector('.notification')
+
+        toastDisallowElement.classList.remove('is-open')
+        toastDisallowElement.classList.add('is-open')
+
+        notificationElement.textContent = notification
+        trustCtaElement.classList.add('visually-hidden')
+
+        setTimeout(() => {
+            toastDisallowElement.classList.remove('is-open')
+        }, 3000)
+
+        localStorage.setItem('isAllow', false)
+        localStorage.setItem('hideTrustCta', true)
+    })
+
+    allowButton.addEventListener('click', () => {
+        const notificationElement = toastAllowElement.querySelector('.notification')
         
-        toastElement.classList.add('is-open')
+        toastAllowElement.classList.remove('is-open')
+        toastAllowElement.classList.add('is-open')
+
+        notificationElement.textContent = 'ALLOW'
+        trustCtaElement.classList.add('visually-hidden')
+
+        setTimeout(() => {
+            toastAllowElement.classList.remove('is-open')
+        }, 3000)
+
+        localStorage.setItem('isAllow', true)
+        localStorage.setItem('hideTrustCta', true)
     })
 }
